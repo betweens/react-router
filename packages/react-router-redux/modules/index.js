abbcc403011a54975ad20a922ae8c81852ed2640
humingfei526 <humingfei526@lu.com>
2017-12-11 17:02:05 +0800

    文件目录结构

---
+++ packages/react-router-redux/modules/index.js
@@ -0,0 +1,9 @@
+export ConnectedRouter from './ConnectedRouter'
+export { getLocation, createMatchSelector } from './selectors'
+export { LOCATION_CHANGE, routerReducer } from './reducer'
+export {
+  CALL_HISTORY_METHOD,
+  push, replace, go, goBack, goForward,
+  routerActions
+} from './actions'
+export routerMiddleware from './middleware'
